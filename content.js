9d216ecedf37a0343555a67183326d0f15a884e2
Niño Ben Payoyo <ninoben@freelancer.com>
2016-04-30 09:25:32 +0800

    for loop element visible check

--- content.js
+++ content.js
@@ -19,8 +19,14 @@ chrome.runtime.onMessage.addListener(
       var contents = document.getElementById('main');
       console.log(contents);
 
-      var text = '${main}    Execute Javascript    return document.getElementById("main")\n\n';
-      text += loopThroughDOM(contents, '${main}');
+      var text = "Set Suite Variable    ${main}    css=[id='main'] \n" +
+                 "   @{elements}    Create List    ${main}\n\n";
+
+      text += loopThroughDOM(contents, '${main}', false);
+
+      text += '\n:   FOR    ${element}    IN    @{elements}\n' +
+              '\\       Run And Wait Until Keyword Succeeds    Element Should Be Visible    ${main}\n' +
+              '\\       ${elementVisibility}   Run Keyword And Return Status    Element Should Be Visible    ${main}\n';
 
       // Acknowledge request
       sendResponse(text);
@@ -31,21 +37,25 @@ chrome.runtime.onMessage.addListener(
 function getAssertAttributes(element, parent) {
   var text = '';
   if (element.attributes.length == 0) {
-    return text;
+    return {};
   }
 
-  var selector = '';
-  for (var i = 0; i < element.attributes.length; i++) {
-    var attr = element.attributes[i];
-    if (i == 0) {
-      selector = '[' + attr.name + '="' + attr.value + '"]';
-    }
-    text += '${attribValue}    Get Element Attribute    css=' + parent + ' ' + selector + '@' + attr.name + '\n';
-    text += 'Should Be Equal    ${attribValue}    ' + attr.value + '\n\n';
-  }
+  // var selector = '';
+  // for (var i = 0; i < element.attributes.length; i++) {
+  //   var attr = element.attributes[i];
+  //   text += '${attribValue}    Get Element Attribute    css=' + parent +  + '@' + attr.name + '\n';
+  //   text += 'Should Be Equal    ${attribValue}    ' + attr.value + '\n\n';
+
+  //   if (i == 0) {
+  //     selector = '[' + attr.name + '="' + attr.value + '"]';
+  //   }
+  // }
+  var attr = element.attributes[0];
+  var selector = parent + ' [' + attr.name + '="' + attr.value + '"]';
+  text += '   Append To List    ${elements}    ' + selector + '\n';
 
   return {
-    selector: parent + ' ' + selector,
+    selector: selector,
     text: text
   };
 }
@@ -65,7 +75,7 @@ function getAssertStyles(element) {
   return text;
 }
 
-function loopThroughDOM(element, parent) {
+function loopThroughDOM(element, parent, append) {
   var text = '';
 
   // exclude blacklisted tags ...
@@ -73,16 +83,19 @@ function loopThroughDOM(element, parent) {
     return text;
   }
 
-  var attr = getAssertAttributes(element, parent);
-  if (attr.text !== '') {
-    text += attr.text;
-    // text += getAssertStyles(element);
+  var selector = parent;
+  if (append) {
+    var attr = getAssertAttributes(element, parent);
+    if (attr.selector && attr.text) {
+      selector = attr.selector;
+      text += attr.text;
+      // text += getAssertStyles(element);
+    }
   }
-
 
   for (var i = 0; i < element.children.length; i++) {
     var child = element.children[i];
-    text += loopThroughDOM(child, attr.selector);
+    text += loopThroughDOM(child, selector, true);
   }
 
   return text;
